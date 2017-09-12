840f42f724ae39ee696c2fe2855972ac03de6b81
davemflick <davemflick@yahoo.com>
2017-09-12 09:35:13 -0400

    added toys to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,6 +15,9 @@ export default class Navbar extends Component{
 			        <a className="nav-link" href="#">About <span className="sr-only">(current)</span></a>
 			      </li>
 			      <li className="nav-item">
+			        <a className="nav-link" href="#">Toys</a>
+			      </li>
+			      <li className="nav-item">
 			        <a className="nav-link" href="#">Events</a>
 			      </li>
 			      <li className="nav-item">
