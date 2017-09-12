042fe40cde6ef5fff5a480c13915dcdf045d2978
davemflick <davemflick@yahoo.com>
2017-09-12 11:09:52 -0400

    not sure

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import Navbar from './components/Navbar';
+import Banner from './components/Banner';
 
 const App = function(){
 	return(
 		<div>
 			<Navbar />
+			<Banner />
 		</div>
 	)
 }
