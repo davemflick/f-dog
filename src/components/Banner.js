700d6619400dc0f7cbe6e04252e1ee7b4266b51a
davemflick <davemflick@yahoo.com>
2017-09-12 17:26:48 -0400

    not sure what i changed

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,7 +4,11 @@ import React, { Component } from 'react';
 export default function Banner(){
 	return(
 		<div className="bannerContainer">
-			<img src={require('../../public/images/gallery/charlie5.jpg')} className="img-fluid" alt="Responsive image" />
+			<img src={require('../../public/images/gallery/macCharCity1.jpg')} className="img-fluid" alt="Responsive image" />
+			<div className="banText">
+				<h3> BEACUSE FARTS ARE FUNNY </h3>
+				<h4> and every pet deserves a home </h4>
+			</div>
 		</div>
 	)
 }
