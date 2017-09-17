63cbe89d48bef770ed710d48ea809391e4f181d3
davemflick <davemflick@yahoo.com>
2017-09-17 08:50:47 -0400

    going with youtube video, original file too large

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
 		loaders: [
 		{test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader', query: { presets: ["react", "es2015"]}},
 		{ test: /\.css$/, loader: "style-loader!css-loader?importLoaders=1" },
-		{ test: /\.(png|jpg)$/, loader: 'url-loader?limit=1000000' }
+		{ test: /\.(png|jpg|mp4)$/, loader: 'url-loader?limit=10000000' }
 		]
 	},
 }
