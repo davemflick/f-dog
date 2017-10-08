ca0ecd91b54f0915b0758617fa40923896481180
davemflick <davemflick@yahoo.com>
2017-10-08 15:59:48 -0400

    added toys route

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,17 +13,20 @@ export default class Navbar extends Component{
 			  </button>
 			  <div className="collapse navbar-collapse" id="navbarSupportedContent">
     			<ul className="navbar-nav">
-			      <li className="nav-item active">
-			        <a className="nav-link" href="#">About <span className="sr-only">(current)</span></a>
+			      <li className="nav-item">
+			        <a title="About-Page" className="nav-link" href="#">About <span className="sr-only">(current)</span></a>
 			      </li>
 			      <li className="nav-item">
-			        <a className="nav-link" href="#">Gallery</a>
+			        <a title="Toys Link" className="nav-link" href="/toys">Toys <span className="sr-only">(current)</span></a>
 			      </li>
 			      <li className="nav-item">
-			        <a className="nav-link" href="#">Events</a>
+			        <a title="Gallery Page" className="nav-link" href="#">Gallery</a>
 			      </li>
 			      <li className="nav-item">
-			        <a className="nav-link" href="#">FAQ's</a>
+			        <a title="Events Page" className="nav-link" href="#">Events</a>
+			      </li>
+			      <li className="nav-item">
+			        <a title="FAQ page" className="nav-link" href="#">FAQ's</a>
 			      </li>
 			    </ul>
 			   </div>
