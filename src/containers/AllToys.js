c90739f82d5d7fe03e50352c56940f6ff0df0d6d
davemflick <davemflick@yahoo.com>
2017-10-10 19:48:32 -0400

    finally found way to get local images

--- src/containers/AllToys.js
+++ src/containers/AllToys.js
@@ -1,9 +1,41 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 
 export default class AllToys extends Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			toys: []
+		}
+	}
+
+	componentWillReceiveProps(nextProps){
+		if(nextProps != this.props){
+	
+			this.setState(nextProps);
+		}
+	}
+
+	createEachToyInstance(){
+		let toyData = this.state.toys;
+		let toys = toyData.map((t, i)=>{
+			return(
+				<div className="single-toy col-md-6" key={"t-" + i}>
+					<h2> {t.name}</h2>
+					<p> {t.description} </p>
+					<img className="img img-fluid" src={require("../../public/images/toys/" + t.images[0])} />
+				</div>
+			)
+		});
+		return toys;
+	}
+
 	render(){
 		return(
-			<div> All Toys Page </div>
+			<div className="row">
+				{this.createEachToyInstance()}
+			</div>
 		)
 	}
 }
