6a89588018eea352ea9ac45a44123ac29cffe030
davemflick <davemflick@yahoo.com>
2017-10-22 11:30:22 -0400

    needed smaller, single, and centered

--- src/components/Video.js
+++ src/components/Video.js
@@ -3,20 +3,15 @@ import React, { Component } from 'react';
 export default function Video(props){
 	return(
 		<div className="video-container">
-		 <div className="row align-items-center">
-			<div className="videoDiv embed-responsive embed-responsive-21by9 col-sm-6">
+		 <div className="container">
+		 <div className="row justify-content-center">
+			<div className="videoDiv embed-responsive embed-responsive-21by9 col-12 col-lg-10">
 			  <iframe src="https://www.youtube.com/embed/i3fi_RqpEOo?ecver=2&showinfo=0" 
 			        className="embed-responsive-item"
 					allowFullScreen>
 			  </iframe>
 			</div>
-			<div className="video-info col-sm-6">
-				<h2> Our Products </h2>
-				<h5>
-				<b>5% of profits donated for homeless pests</b>
-				We all love our fur babies and there are too many pets out there with out a home! 5% of profits will be donated to small, non-profit organizations that protect the welfare of these animals and make sure they find their forever homes!
-				</h5>
-			</div>
+		 </div>
 		 </div>
 		</div>
 	)
