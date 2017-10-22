e777d55265ec481dbfd5f15d5cb9a9a909353d41
davemflick <davemflick@yahoo.com>
2017-10-22 11:29:48 -0400

    added info and hidden fixed banner

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import Banner from '../components/Banner';
 import Video from '../components/Video';
+import HomeInfo from '../components/Home-Info'
 
 
 const HomePage = function(){
 	return(
 		<div>
+			<div className="fixed-banner">
+				&nbsp;
+			</div>
 			<Banner />
 			<h1 className="titleHeader"> The Farting Dog Company </h1>
 			<Video />
+			<HomeInfo />
 		</div>
 	)
 }
