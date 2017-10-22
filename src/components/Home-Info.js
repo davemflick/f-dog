d3094873b7911dc0786d19fdb5c537c455b1dace
davemflick <davemflick@yahoo.com>
2017-10-22 11:27:28 -0400

    added info to homepage

---
+++ src/components/Home-Info.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+const HomeInfo = function(){
+	return(
+		<div className="home-info">
+			<div className="container">
+				<h2>Who We Are</h2>
+				<h5>A unique and humorous line of dog toys and supplies. A portion of profits are donated to roganizations that support the well-being of homeless animals.</h5>
+			</div>
+		</div>
+	)
+}
+
+module.exports = HomeInfo;
